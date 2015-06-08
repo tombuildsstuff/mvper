8054f19aa16dea2680baa20d5b1a09e245f99500
tombuildsstuff <git@ibuildstuff.co.uk>
2015-06-08 22:19:31 +0100

    initial commit

---
+++ MVPer/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  MVPer
+//
+//  Created by Tom Harvey on 08/06/2015.
+//  Copyright (c) 2015 WeBuildStuff. All rights reserved.
+//
+
+import Foundation
+
+println("Does Jonathon Channon Have an MVP yet?");
+
+while (true) {
+    println("Not yet..");
+    sleep(1);
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+MVPer
+=====
+
+A simple console app to demonstrate how to build CLI apps using Swift
+
+Get Started
+===========
+
+ - Open Xcode
+ - File -> New -> Project
+ - OS X -> Appliction -> Command Line Tool
+ - Build
+ - ???
+ - Profit
